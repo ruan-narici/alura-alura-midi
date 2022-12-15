68509ed5941ba7556274f72544d72748ee7d49e7
Ruan Narici <ruannarici@hotmail.com>
2022-12-15 18:06:28 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@ const listaTeclas = document.querySelectorAll('.tecla');
 const listaSons = document.querySelectorAll('body>audio');
 
 function tocaSom(idElementAudio) {
-    listaSons[idElementAudio].play();
+    if (listaSons[idElementAudio] != null) {
+        listaSons[idElementAudio].play();
+    }
+    else {
+        console.log('Elemento não identificado');
+    }
 }
 
 for (let i = 0; i < listaTeclas.length; i++) {
     listaTeclas[i].onclick = function () {
-        tocaSom(i);
+            tocaSom(i);
+    } 
+//add class ativa
+    listaTeclas[i].onkeydown = function (event) {
+        if (event.code === 'Enter' || 
+            event.code === 'Space') {
+                listaTeclas[i].classList.add('ativa');
+    }
+    }
+//remove class ativa
+    listaTeclas[i].onkeyup = function () {
+        listaTeclas[i].classList.remove('ativa');
     }
 }
