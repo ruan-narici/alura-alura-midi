58a89a9a5b2added26d702125472c758771f9d35
Ruan Narici <ruannarici@hotmail.com>
2022-12-15 16:06:24 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 const listaTeclas = document.querySelectorAll('.tecla');
-
 const listaSons = document.querySelectorAll('body>audio');
 
+function tocaSom(idElementAudio) {
+    listaSons[idElementAudio].play();
+}
+
+for (let i = 0; i < listaTeclas.length; i++) {
+    listaTeclas[i].onclick = function () {
+        tocaSom(i);
+    }
+}
